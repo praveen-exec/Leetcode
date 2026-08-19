875cd2f3efb7c71c7f2869ce07a954563e57e97b
PRAVEEN SINGH <137392913+praveen-exec@users.noreply.github.com>
2026-08-19 17:54:46 +0530

    Time: 6 ms (19.35%), Space: 18.1 MB (31.38%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,28 +1,37 @@
 class Solution {
-public:
+   public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        //Approach 1 ==> Map
+        // Approach 2 ==> Min Heap
         map<int, int> mp;
 
+        typedef pair<int, int> P;
+
         // Store frequency
-        for(int x : nums) {
+        for (int x : nums) {
             mp[x]++;
         }
 
-        vector<pair<int,int>> v;
+        // min heap
+        priority_queue<P, vector<P>, greater<P>> pq;
 
-        // {frequency, element}
-        for(auto &p : mp) {
-            v.push_back({p.second, p.first});
+        for (auto& p : mp) {
+            int element = p.first;
+            int freq = p.second;
+
+            pq.push({freq, element});
+
+            if (pq.size() > k) pq.pop();
         }
-
-        // Sort by frequency in descending order
-        sort(v.begin(), v.end(), greater<pair<int,int>>());
+        // Freq -element
+        // 3 - 1
+        // 2 - 2
+        // 1 - 3
 
         vector<int> ans;
 
-        for(int i = 0; i < k; i++) {
-            ans.push_back(v[i].second);
+        while (!pq.empty()) {
+            ans.push_back(pq.top().second);
+            pq.pop();
         }
 
         return ans;
