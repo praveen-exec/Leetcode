ca5714d238c3fcaebb284d0eef56de2d2444d2c9
PRAVEEN SINGH <137392913+praveen-exec@users.noreply.github.com>
2026-08-19 17:58:38 +0530

    update 0347-top-k-frequent-elements.cpp

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,7 +1,41 @@
+ //Approach 1 ==> Map
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+       
+        unordered_map<int, int> mp;
+
+        // Store frequency
+        for(int x : nums) {
+            mp[x]++;
+        }
+
+        vector<pair<int,int>> v;
+
+        // {frequency, element}
+        for(auto &p : mp) {
+            v.push_back({p.second, p.first});
+        }
+
+        // Sort by frequency in descending order
+        sort(v.begin(), v.end(), greater<pair<int,int>>());
+
+        vector<int> ans;
+
+        for(int i = 0; i < k; i++) {
+            ans.push_back(v[i].second);
+        }
+
+        return ans;
+    }
+};
+
+
+//Aproach 2 ==> Min Heap
 class Solution {
    public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        // Approach 2 ==> Min Heap
+        
         map<int, int> mp;
 
         typedef pair<int, int> P;
