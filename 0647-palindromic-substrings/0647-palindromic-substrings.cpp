63f7b04acdb87242a29a9563f14eadb808125a18
PRAVEEN SINGH <137392913+praveen-exec@users.noreply.github.com>
2026-08-29 21:22:20 +0530

    Time: 1044 ms (10.59%), Space: 463.2 MB (5.67%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,29 +1,35 @@
 class Solution {
 public:
+
+    bool ispalindrome(string &s) {
+        int start = 0;
+        int end = s.length() - 1;
+
+        while (start < end) {
+            if (s[start++] != s[end--])
+                return false;
+        }
+
+        return true;
+    }
+
     int countSubstrings(string s) {
-        int n = s.length();
+
         int count = 0;
 
-        // Check for all centers
-        for(int center = 0; center < n; center++) {
-            
-            // Odd length palindrome
-            int left = center, right = center;
-            while(left >= 0 && right < n && s[left] == s[right]) {
-                count++;
-                left--;
-                right++;
-            }
+        for (int i = 0; i < s.length(); i++) {
 
-            // Even length palindrome
-            left = center;
-            right = center + 1;
-            while(left >= 0 && right < n && s[left] == s[right]) {
-                count++;
-                left--;
-                right++;
+            for (int j = i; j < s.length(); j++) {
+
+                // substring(start, length)
+                string temp = s.substr(i, j - i + 1);
+
+                if (ispalindrome(temp)) {
+                    count++;
+                }
             }
         }
-        return count;  
+
+        return count;
     }
 };
