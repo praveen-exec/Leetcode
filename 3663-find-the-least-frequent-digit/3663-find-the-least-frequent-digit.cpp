184de98d6e7490c022a0b49ade339dc3d70a4fe2
PRAVEEN SINGH <137392913+praveen-exec@users.noreply.github.com>
2026-08-29 20:19:47 +0530

    Update 3663-find-the-least-frequent-digit.cpp
    Added comments to clarify logic for finding least frequent digit.

--- 3663-find-the-least-frequent-digit/3663-find-the-least-frequent-digit.cpp
+++ 3663-find-the-least-frequent-digit/3663-find-the-least-frequent-digit.cpp
@@ -11,8 +11,12 @@ public:
         }
 
         int mini = INT_MAX;
-
+        
         for (auto p : mp) {
+            //agar same bhi hoga then isme handle ho jaayega because we have used ordered map 
+            // 1->2
+            // 2->2
+            // mini=1 & ans =1 after that 2<2 = false and ans=1 hi hoga
             if (p.second < mini) {
                 mini = p.second;
                 ans = p.first;
