0547d87394dc9612729abf68a734417d5637452a
PRAVEEN SINGH <137392913+praveen-exec@users.noreply.github.com>
2026-08-29 20:59:41 +0530

    Time: 1044 ms (10.59%), Space: 463.2 MB (5.67%) - LeetHub

---
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+
+    bool ispalindrome(string &s) {
+        int start = 0;
+        int end = s.length() - 1;
+
+        while (start < end) {
+            if (s[start++] != s[end--])
+                return false;
+        }
+
+        return true;
+    }
+
+    int countSubstrings(string s) {
+
+        int count = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+
+            for (int j = i; j < s.length(); j++) {
+
+                // substring(start, length)
+                string temp = s.substr(i, j - i + 1);
+
+                if (ispalindrome(temp)) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+};
