3803b517b9557834e06ca76f2eaf6c93a75b704e
PRAVEEN SINGH <137392913+praveen-exec@users.noreply.github.com>
2026-09-15 20:58:50 +0530

    Time: 0 ms (100%), Space: 12 MB (29.33%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+
+        unordered_map<char, int> mp;
+
+        // Magazine ke har character ka count store karo
+        for(char x : magazine) {
+            mp[x]++;
+        }
+
+        // Ransom note ke characters check karo
+        for(int i = 0; i < ransomNote.length(); i++) {
+
+            // Character present nahi hai
+            if(mp.find(ransomNote[i]) == mp.end())
+                return false;
+
+            if(mp[ransomNote[i]] == 0)
+                return false;
+
+            mp[ransomNote[i]]--;
+        }
+
+        return true;
+    }
+};
